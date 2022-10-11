24f714a7fd9cca1a8fbe738b2fa7a3563774fd7d
Thanystos <guichardanthony@live.fr>
2022-10-11 10:31:45 +0200

    Développement du serveur afin que ce dernier : - Normalise notre port d'écoute - Gère les erreurs en les affichants ou affiche le port en cours d'écoute
    Développement de l'application en commençant par :
    - établir la connexion à la bdd
    - permettre les requêtes cross-orign (CORS)

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const bodyParser = require('body-parser'); // On peut utiliser bodyparser pour intercepter le json plutôt qu'express
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Thanystos:Devilplop0@cluster0.asgmhcu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
